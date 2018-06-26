659a5bb2996fb6a5214512c0b12793d86d64a271
Abby Lortz <abilenelortz03@gmail.com>
2018-06-26 15:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+  let drivers = ['Milo', 'Otis', 'Garfield']
+  console.log(drivers)
+  
+// }
+
+// function destructivelyAppendDriver(Ralph) {
+  
+// }
+
+// function destructivelyPrependDriver(Bob) 
